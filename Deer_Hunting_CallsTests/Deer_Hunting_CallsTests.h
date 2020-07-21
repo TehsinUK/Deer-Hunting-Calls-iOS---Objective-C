ab2cea793590dbbdec9eec9cb2687b11422d5ea0
wkb786 <wkb786@gmail.com>
2020-07-22 00:26:44 +0500

    init2

---
+++ Deer_Hunting_CallsTests/Deer_Hunting_CallsTests.h
@@ -0,0 +1,13 @@
+//
+//  Deer_Hunting_CallsTests.h
+//  Deer_Hunting_CallsTests
+//
+//  Created by Naveed on 2/2/13.
+//  Copyright (c) 2013 Naveed. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Deer_Hunting_CallsTests : SenTestCase
+
+@end
---
+++ Deer_Hunting_CallsTests/Deer_Hunting_CallsTests.m
@@ -0,0 +1,32 @@
+//
+//  Deer_Hunting_CallsTests.m
+//  Deer_Hunting_CallsTests
+//
+//  Created by Naveed on 2/2/13.
+//  Copyright (c) 2013 Naveed. All rights reserved.
+//
+
+#import "Deer_Hunting_CallsTests.h"
+
+@implementation Deer_Hunting_CallsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Deer_Hunting_CallsTests");
+}
+
+@end
